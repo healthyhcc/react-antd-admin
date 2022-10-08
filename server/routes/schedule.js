5676a15fc42ea8e2d7e62bd38572b47e56f18b04
healthyhcc <healthyhcc@gmail.com>
2022-10-08 09:37:29 +0800

    remove: remove unused packages

--- server/routes/schedule.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const schedule = require('node-schedule');
-
-router.post("/addTask", (request, response) => {
-    const job = schedule.scheduleJob()
-});
-
-module.exports = router;
